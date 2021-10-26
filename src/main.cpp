392af588d4cbd33a77c6e4df3e59762eb05fab93
Richard Lyon <richlyon@mac.com>
2021-10-26 10:40:05 +0100

    Squashed commit of the following:
    commit 1aa8bff49a1971cfdef49d2ae4d9d5f31818f915
    Author: Richard Lyon <richlyon@mac.com>
    Date:   Tue Oct 26 10:38:50 2021 +0100
    
        scd30 and OLED display working
    
    commit 222bb6d328bf3f95d462bd0e3efaf7e65e6a8363
    Author: Richard Lyon <richlyon@mac.com>
    Date:   Tue Oct 26 10:32:10 2021 +0100
    
        Sparkfun basic reading working

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
 #include <Arduino.h>
 
 #include "heltec.h"
+#include "SparkFun_SCD30_Arduino_Library.h"
 #include <kwWiFi.h>
+
+// Required to avoid conflict with OLED
+// See: https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series/issues/62
+#define PIN_SDA 4
+#define PIN_SCL 15
+
+SCD30 airSensor;
 
 void setup() {
   pinMode(LED,OUTPUT);
 	digitalWrite(LED,LOW);
   
   Serial.begin(115200);
+  Wire.begin(PIN_SDA, PIN_SCL);
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
 
   WIFISetUp();
 
+  if (airSensor.begin() == false)
+  {
+    Serial.println("Air sensor not detected. Please check wiring. Freezing...");
+    while (1)
+      ;
+  }
+
 }
 
 void loop() {
-  ;
+  if (airSensor.dataAvailable())
+  {
+    Serial.print("co2(ppm):");
+    Serial.print(airSensor.getCO2());
+
+    Serial.print(" temp(C):");
+    Serial.print(airSensor.getTemperature(), 1);
+
+    Serial.print(" humidity(%):");
+    Serial.print(airSensor.getHumidity(), 1);
+
+    Serial.println();
+  }
+  delay(500);
 }
