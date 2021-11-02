8a534cbfca1d5307112254a94e0bc0b5b3929608
Richard <richlyon@mac.com>
2021-11-02 07:45:31 +0000

    Refactor to use kwHeltecWifikit32 v1.0.4 and kwSCD30 v1.0.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,54 @@
 #include <Arduino.h>
-#include <kwTimer.h>
 #include <kwHeltecWifikit32.h>
-#include <secrets.h>
+#include <kwNeoTimer.h>
 #include <kwSCD30.h>
+#include <secrets.h>
 
 #define SENSOR_TYPE "energy"
-#define FIRMWARE_VERSION "2.0.0"
-
-// OLED pins
-#define PIN_RST 16
-#define PIN_SDA 4
-#define PIN_SCL 15
-
-// Network credentials
-// #define WIFI_SSID xxx
-// #define WIFI_PASSWORD xxx
-#define MQTT_HOST IPAddress(192, 168, 1, 240) // Mac Mini M1
-#define MQTT_PORT 1883
+#define FIRMWARE_VERSION "2.1.0"
 #define TOPIC_ROOT "kw_sensors"
+#define TEMPERATURE_OFFSET 1.3
 
 uint8_t temperatureField;
 uint8_t humidityField;
 uint8_t co2Field;
+uint8_t temperatureOffsetCommandField;
 
-kwHeltecWifikit32 heltec;
+struct HeltecConfig config = 
+{
+  .ssid = WIFI_SSID,
+  .pwd = WIFI_PASSWORD,
+  .mqtt_host = IPAddress(192, 168, 1, 240), // Mac Mini M1
+  .rotateDisplay = true,
+  .firmwareVersion = FIRMWARE_VERSION,
+  .topicRoot = TOPIC_ROOT
+};
+
+kwHeltecWifikit32 heltec{ config };
 kwSCD30 scd30;
-kwTimer publishDataTimer = kwTimer();
+kwNeoTimer publishDataTimer = kwNeoTimer();
 
 void setup() 
 {
     Serial.begin(115200);
-    heltec.initDisplay(PIN_RST, PIN_SDA, PIN_SCL);
-
-    temperatureField = heltec.registerDataTopic("Temp", "degC", "temperature", "SCD30");
-    humidityField = heltec.registerDataTopic("Hum", "%", "humidity", "SCD30");
-    co2Field = heltec.registerDataTopic("CO2", "ppm", "co2", "SCD30");
     
+    temperatureField = heltec.registerField("Temp", "degC", "temperature", "SCD30");
+    humidityField = heltec.registerField("Hum", "%", "humidity", "SCD30");
+    co2Field = heltec.registerField("CO2", "ppm", "co2", "SCD30");
+    
+    heltec.init();
+    scd30.start( TEMPERATURE_OFFSET );
+
     Serial.printf("\n------------------%s sensor------------------------------------------------\n\n", SENSOR_TYPE);
     Serial.printf("%-12s : %s\n", "Firmware", FIRMWARE_VERSION);
     Serial.printf("%-12s : %s\n", "Device ID", heltec.deviceID);
+    Serial.printf("%-12s : %s\n", "SCD30", scd30.hasSCD30() ? "OK" : "Not found");
+    Serial.printf("%-12s : %.1f degC\n", "Offset", scd30.temperatureOffset());
+    Serial.printf("%-12s : %s\n", "Status", heltec.metaTopics[heltec.statusTopicID].c_str());
     for (int i=0; i < heltec.dataTopics.size(); i++)
     {
       Serial.printf("%-12s : %s\n", heltec.dataTopics[i].fieldName.c_str(), heltec.dataTopics[i].topicString.c_str());
     }
-    Serial.printf("%-12s : %s\n", "status", heltec.metaTopics[heltec.statusTopicID].c_str());
-    
-    heltec.initWiFi(WIFI_SSID, WIFI_PASSWORD);
-    heltec.initMTTQ(MQTT_HOST, TOPIC_ROOT);
-
-    scd30.start();
-    
     publishDataTimer.set(1000);
   }
 
@@ -61,8 +60,9 @@ void loop()
       heltec.publish(humidityField, scd30.humidity());
       heltec.publish(co2Field, scd30.co2());
 
-      heltec.display();
+      heltec.update(temperatureField, scd30.temperature());
+      heltec.update(humidityField, scd30.humidity());
+      heltec.update(co2Field, scd30.co2());
   }
-  
   heltec.run();
 }
